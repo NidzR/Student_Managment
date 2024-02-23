35bcd57170841ec519b58454e124df463ee049d4
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-02-23 22:23:04 +0500

    CLI Project student_management_system

---
+++ stu.js
@@ -0,0 +1,58 @@
+// class School {
+//    name: string;
+export {};
+//    students:any[]=[];
+//    teachers:any[]=[];
+//    addStudent(stdObj:any){
+//       this.students.push(stdObj)
+//    }
+//    addTeacher(tedObj:any){
+//       this.teachers.push(tedObj)
+//    }
+//    constructor(name:string){
+//       this.name = name;
+//    }
+// }
+// class Student {
+//    name: string;
+//    age: number;
+//    schoolName: string;
+//    constructor(name:string,age:number,schoolName:string){
+//          this.name = name;
+//          this.age = age;
+//          this.schoolName = schoolName;
+//    }
+// }
+// class Teacher1 {
+//    name: string;
+//    age: number;
+//    schoolName: string;
+//    constructor(name:string,age:number,schoolName:string){
+//          this.name = name;
+//          this.age = age;
+//          this.schoolName = schoolName;
+//    }
+// }
+// class Teacher extends Student {}
+// let school1 = new School("Premier School")
+// let school2 = new School("Happy Palace")
+// let school3 = new School("Karachi Academy")
+// // student
+// let s1 = new Student("Azhar", 25,school1.name)
+// let s2 = new Student("Ahsen", 10,school2.name)
+// let s3 = new Student("Rizwan", 45,school3.name)
+// // teacher
+// let t1 = new Teacher("Nida", 33,school1.name)
+// let t2 = new Teacher("Azlan", 30,school2.name)
+// let t3 = new Teacher("Azhar", 40,school3.name)
+// // Students
+// school1.addStudent(s1)
+// school2.addStudent(s2)
+// school3.addStudent(s3)
+// // teachers
+// school1.addTeacher(t1)
+// school2.addTeacher(t2)
+// school3.addTeacher(t3)
+// console.log(school1);
+// console.log(school2);
+// console.log(school3);
